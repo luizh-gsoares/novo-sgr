d5e7fb808785467f0ffdab8599255bf33d67ae6a
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-11-08 11:57:28 -0300

    Iniciado as tratativas da criação da classe Empregado

---
+++ src/java/dao/EmpregadoDAO.java
@@ -0,0 +1,192 @@
+
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import model.Empregado;
+
+public class EmpregadoDAO {
+
+    public boolean cadastraEmpregado(Empregado empregado) {
+        String insertTableSQL = "INSERT INTO empregado "
+                + " (nome, matricula, nomeSocial, nacionalidade, naturalidade, uf, dataNasc, sexo, estadoCivil, rg, dataxpedicao, orgaoExpedidor, cpf, zonaEleitoral, secao, tituloEleitoral, email, nomeConjugue, nomePai, nomeMae, especial)"
+                + " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = DbConnect.getConexao().prepareStatement(insertTableSQL);
+            preparedStatement.setString(1, empregado.getNome());
+            preparedStatement.setString(2, empregado.getMatricula());
+            preparedStatement.setString(3, empregado.getNomeSocial());
+            preparedStatement.setInt(4, empregado.getNacionalidade());
+            preparedStatement.setInt(5, empregado.getNaturalidade());
+            preparedStatement.setString(6, empregado.getUf());
+            preparedStatement.setString(7, empregado.getDataNasc());
+            preparedStatement.setInt(8, empregado.getSexo());
+            preparedStatement.setString(9, empregado.getEstadoCivil());
+            preparedStatement.setString(10, empregado.getRg());
+            preparedStatement.setString(11, empregado.getDataExpedicao());
+            preparedStatement.setString(12, empregado.getOrgaoExpedidor());
+            preparedStatement.setString(13, empregado.getCpf());
+            preparedStatement.setString(14, empregado.getZonaEleitoral());
+            preparedStatement.setString(15, empregado.getSecao());
+            preparedStatement.setString(16, empregado.getTituloEleitoral());
+            preparedStatement.setString(17, empregado.getEmail());
+            preparedStatement.setString(18, empregado.getNomeConjugue());
+            preparedStatement.setString(19, empregado.getNomePai());
+            preparedStatement.setString(20, empregado.getNomeMae());
+            preparedStatement.setInt(21, empregado.getEspecial());
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public boolean alteraEmpregado(Empregado empregado) {
+        String updateTableSQL = "UPDATE empregado "
+                + " SET nome = ?, matricula = ?, nomeSocial = ?, nacionalidade = ?, naturalidade = ?, uf = ?, dataNasc = ?, sexo = ?, estadoCivil = ?, rg = ?, dataxpedicao = ?, orgaoExpedidor = ?, cpf = ?, zonaEleitoral = ?, secao = ?, tituloEleitoral = ?, email = ?, nomeConjugue = ?, nomePai = ?, nomeMae = ?, especial = ? "
+                + " WHERE idEmpregado = ?";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = DbConnect.getConexao().prepareStatement(updateTableSQL);
+            preparedStatement.setString(1, empregado.getNome());
+            preparedStatement.setString(2, empregado.getMatricula());
+            preparedStatement.setString(3, empregado.getNomeSocial());
+            preparedStatement.setInt(4, empregado.getNacionalidade());
+            preparedStatement.setInt(5, empregado.getNaturalidade());
+            preparedStatement.setString(6, empregado.getUf());
+            preparedStatement.setString(7, empregado.getDataNasc());
+            preparedStatement.setInt(8, empregado.getSexo());
+            preparedStatement.setString(9, empregado.getEstadoCivil());
+            preparedStatement.setString(10, empregado.getRg());
+            preparedStatement.setString(11, empregado.getDataExpedicao());
+            preparedStatement.setString(12, empregado.getOrgaoExpedidor());
+            preparedStatement.setString(13, empregado.getCpf());
+            preparedStatement.setString(14, empregado.getZonaEleitoral());
+            preparedStatement.setString(15, empregado.getSecao());
+            preparedStatement.setString(16, empregado.getTituloEleitoral());
+            preparedStatement.setString(17, empregado.getEmail());
+            preparedStatement.setString(18, empregado.getNomeConjugue());
+            preparedStatement.setString(19, empregado.getNomePai());
+            preparedStatement.setString(20, empregado.getNomeMae());
+            preparedStatement.setInt(21, empregado.getEspecial());
+            preparedStatement.setInt(22, empregado.getIdEmpregado());
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public boolean excluiEmpregado(Integer toDelete) {
+        String deleteTableSQL = "DELETE FROM empregado WHERE idEmpregado = ?";
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = DbConnect.getConexao().prepareStatement(deleteTableSQL);
+            preparedStatement.setInt(1, toDelete);
+            preparedStatement.execute();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public Empregado procuraEmpregadoPeloID(Integer idEmpregado) {
+
+        try {
+            String sql = "SELECT * FROM empregado WHERE idEmpregado = ?";
+            PreparedStatement con = DbConnect.getConexao().prepareStatement(sql);
+            con.setInt(1, idEmpregado);
+            ResultSet rs = con.executeQuery();
+
+            Empregado empr = new Empregado();
+            if (rs.next()) {
+                empr.setIdEmpregado(rs.getInt("idEmpregado"));
+                empr.setNome(rs.getString("nome"));
+                empr.setMatricula(rs.getString("matricula"));
+                empr.setNomeSocial(rs.getString("nomeSocial"));
+                empr.setNacionalidade(rs.getInt("nacionalidade"));
+                empr.setNaturalidade(rs.getInt("naturalidade"));
+                empr.setUf(rs.getString("uf"));
+                empr.setDataNasc(rs.getString("dataNasc"));
+                empr.setSexo(rs.getInt("sexo"));
+                empr.setEstadoCivil(rs.getString("estadoCivil"));
+                empr.setRg(rs.getString("rg"));
+                empr.setDataExpedicao(rs.getString("dataExpedicao"));
+                empr.setOrgaoExpedidor(rs.getString("orgaoExpedidor"));
+                empr.setCpf(rs.getString("cpf"));
+                empr.setZonaEleitoral(rs.getString("zonaEleitoral"));
+                empr.setSecao(rs.getString("secao"));
+                empr.setTituloEleitoral(rs.getString("tituloEleitoral"));
+                empr.setEmail(rs.getString("email"));
+                empr.setNomeConjugue(rs.getString("nomeConjugue"));
+                empr.setNomePai(rs.getString("nomePai"));
+                empr.setNomeMae(rs.getString("nomeMae"));
+                empr.setEspecial(rs.getInt("especial"));
+            }
+
+            rs.close();
+            con.close();
+            return empr;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<Empregado> procuraTodosEmpregados() {
+
+        try {
+            String sql = "SELECT * FROM empregado";
+            PreparedStatement con = DbConnect.getConexao().prepareStatement(sql);
+
+            ResultSet rs = con.executeQuery();
+
+            ArrayList<Empregado> listaEmpregados = new ArrayList<>();
+
+            while (rs.next()) {
+                Empregado empr = new Empregado();
+                empr.setIdEmpregado(rs.getInt("idEmpregado"));
+                empr.setNome(rs.getString("nome"));
+                empr.setMatricula(rs.getString("matricula"));
+                empr.setNomeSocial(rs.getString("nomeSocial"));
+                empr.setNacionalidade(rs.getInt("nacionalidade"));
+                empr.setNaturalidade(rs.getInt("naturalidade"));
+                empr.setUf(rs.getString("uf"));
+                empr.setDataNasc(rs.getString("dataNasc"));
+                empr.setSexo(rs.getInt("sexo"));
+                empr.setEstadoCivil(rs.getString("estadoCivil"));
+                empr.setRg(rs.getString("rg"));
+                empr.setDataExpedicao(rs.getString("dataExpedicao"));
+                empr.setOrgaoExpedidor(rs.getString("orgaoExpedidor"));
+                empr.setCpf(rs.getString("cpf"));
+                empr.setZonaEleitoral(rs.getString("zonaEleitoral"));
+                empr.setSecao(rs.getString("secao"));
+                empr.setTituloEleitoral(rs.getString("tituloEleitoral"));
+                empr.setEmail(rs.getString("email"));
+                empr.setNomeConjugue(rs.getString("nomeConjugue"));
+                empr.setNomePai(rs.getString("nomePai"));
+                empr.setNomeMae(rs.getString("nomeMae"));
+                empr.setEspecial(rs.getInt("especial"));
+                listaEmpregados.add(empr);
+            }
+
+            rs.close();
+            con.close();
+            return listaEmpregados;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
---
+++ src/java/model/Empregado.java
@@ -0,0 +1,67 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Empregado {
+
+    private Integer idEmpregado;
+    private String nome;
+    private String matricula;
+    private String nomeSocial;
+    private Integer nacionalidade;
+    private Integer naturalidade;
+    private String uf;
+    private String dataNasc;
+    private Integer sexo;
+    private String estadoCivil;
+    private String rg;
+    private String dataExpedicao;
+    private String orgaoExpedidor;
+    private String cpf;
+    private String zonaEleitoral;
+    private String secao;
+    private String tituloEleitoral;
+    private String email;
+    private String nomeConjugue;
+    private String nomePai;
+    private String nomeMae;
+    private Integer especial;
+
+    public Empregado(Integer idEmpregado, String nome, String matricula, String nomeSocial, Integer nacionalidade, Integer naturalidade, String uf, String dataNasc, Integer sexo, String estadoCivil, String rg, String dataExpedicao, String orgaoExpedidor, String cpf, String zonaEleitoral, String secao, String tituloEleitoral, String email, String nomeConjugue, String nomePai, String nomeMae, Integer especial) {
+        this.idEmpregado = idEmpregado;
+        this.nome = nome;
+        this.matricula = matricula;
+        this.nomeSocial = nomeSocial;
+        this.nacionalidade = nacionalidade;
+        this.naturalidade = naturalidade;
+        this.uf = uf;
+        this.dataNasc = dataNasc;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+        this.rg = rg;
+        this.dataExpedicao = dataExpedicao;
+        this.orgaoExpedidor = orgaoExpedidor;
+        this.cpf = cpf;
+        this.zonaEleitoral = zonaEleitoral;
+        this.secao = secao;
+        this.tituloEleitoral = tituloEleitoral;
+        this.email = email;
+        this.nomeConjugue = nomeConjugue;
+        this.nomePai = nomePai;
+        this.nomeMae = nomeMae;
+        this.especial = especial;
+    }
+
+    public Empregado() {
+    }
+
+    @Override
+    public String toString() {
+        return "Empregado{" + "idEmpregado=" + idEmpregado + ", nome=" + nome + ", matricula=" + matricula + ", nomeSocial=" + nomeSocial + ", nacionalidade=" + nacionalidade + ", naturalidade=" + naturalidade + ", uf=" + uf + ", dataNasc=" + dataNasc + ", sexo=" + sexo + ", estadoCivil=" + estadoCivil + ", rg=" + rg + ", dataExpedicao=" + dataExpedicao + ", orgaoExpedidor=" + orgaoExpedidor + ", cpf=" + cpf + ", zonaEleitoral=" + zonaEleitoral + ", secao=" + secao + ", tituloEleitoral=" + tituloEleitoral + ", email=" + email + ", nomeConjugue=" + nomeConjugue + ", nomePai=" + nomePai + ", nomeMae=" + nomeMae + ", especial=" + especial + '}';
+    }
+
+}
