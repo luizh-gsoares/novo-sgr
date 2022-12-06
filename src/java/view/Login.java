f885faf76f71be14030d7d998110b19e284f1049
luizh-gsoares <luizhenriquesoares18@hotmail.com>
2022-12-06 10:16:20 -0300

    Implemented error message login

--- src/java/view/Login.java
+++ src/java/view/Login.java
@@ -9,18 +9,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Usuario;
 import dao.UsuarioDAO;
+import java.io.PrintWriter;
 import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class Login extends HttpServlet {
+    private static HttpServletResponse response;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        RequestDispatcher dispatcher = getServletContext()
-                .getRequestDispatcher("/WEB-INF/login.jsp");
-        dispatcher.forward(request, response);
+        HttpSession sessao = request.getSession();
+        sessao.removeAttribute("userLogado");
+        sessao.invalidate();
+        response.sendRedirect("login.jsp");
 
     }
 
@@ -28,6 +31,8 @@ public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        PrintWriter out = response.getWriter();
+        Login.response = response;
         Usuario u = new Usuario();
         u.setLogin(request.getParameter("login"));
         u.setSenha(request.getParameter("senha"));
@@ -35,23 +40,37 @@ public class Login extends HttpServlet {
         String page = "index.jsp";
 
         UsuarioDAO dao = new UsuarioDAO();
-        
+
         if (dao.validar(u)) {
             u = dao.procuraUsuarioPeloID(u.getLogin());
             request.setAttribute("usuario", u);
-            HttpSession ses = request.getSession();
-            ses.setAttribute("usuario",u);
+            HttpSession sessao = request.getSession();
+            sessao.setAttribute("userLogado", u);
             response.sendRedirect("index.jsp");
-            System.out.println("entrei");
+            System.out.println("Usuário logado");
         } else {
             //enviar um atributo msg de erro
+            exibirMensagem("Login ou Senha Inválidos!");
             request.setAttribute("erro", "Usuário ou senha inválida!");
         }
     }
-
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
+    private static void exibirMensagem(String mensagem){
+        try {
+             PrintWriter out = response.getWriter();
+             out.print(
+                "<script type='text/javascript'>" +
+                "alert('" + mensagem + "');"+
+                "history.back();" +
+                "</script>");
+        out.close();
+            
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+            e.printStackTrace();
+        }
+       
+        
+    }
+    
 }
